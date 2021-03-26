14489c9595659d5a3ff1be56e34ab033e4da8f09
usuario <manufer37@gmail.com>
2021-03-26 00:39:32 -0300

    Registro de Usuarios

---
+++ app/src/main/java/com/example/petagram/RegistroUsuarios.java
@@ -0,0 +1,26 @@
+package com.example.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.text.BreakIterator;
+
+public class RegistroUsuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuarios);
+    }
+
+
+}
+
